e69885bf457ed145eb8735479dd2085e457efdd6
Artem <t9_0@bk.ru>
2023-02-01 23:21:51 +0700

    build for server

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,12 @@
 import {defineConfig} from 'vite';
 import laravel from 'laravel-vite-plugin';
+import legacy from '@vitejs/plugin-legacy'
 
 export default defineConfig({
     plugins: [
+        legacy({
+            targets: ['defaults', 'not IE 11'],
+        }),
         laravel({
             input: ['resources/scss/app.scss', 'resources/js/app.js'],
             refresh: true,
